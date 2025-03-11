94c0a8682da292fa710248b9768bfece77aa5e9e
ST10437264 <160749213+ST10437264@users.noreply.github.com>
2025-03-11 10:39:13 +0200

    Added new class
    we are working on classes

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -11,6 +11,13 @@ package com.mycompany.prjclasses;
 public class PrjClasses {
 
     public static void main(String[] args) {
+        
         System.out.println("Hello World! I just did my first GitHub Activity!");
+        
+        newClass nc = new newClass(); // creating an object of newClass
+        
+        nc.helloClass(); // have to use variable name to communicate with the class
+        
+        System.out.println(nc.strSecondClassVar);  // output variable from 2nd class
     }
 }
